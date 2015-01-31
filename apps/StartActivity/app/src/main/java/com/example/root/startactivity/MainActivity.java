aa29b91ee96e578c57bfb65756c0245f1cbd3821
Florian Thomas <flothomas@web.de>
2015-01-31 14:26:05 +0100

    StartActivity added

---
+++ apps/StartActivity/app/src/main/java/com/example/root/startactivity/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.root.startactivity;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startActivity(View view){
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+}
