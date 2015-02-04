46a3ea66345c8225aa92ef114d8b53bda55c7b3d
Florian Thomas <flothomas@web.de>
2015-02-04 17:23:08 +0100

    add sendbroadcast2 - explicit

--- apps/SendBroadcast/app/src/main/java/com/broadcast/unibremen/sendbroadcast/SendBroadcastActivity.java
+++ apps/SendBroadcast/app/src/main/java/com/broadcast/unibremen/sendbroadcast/SendBroadcastActivity.java
@@ -1,13 +1,11 @@
 package com.broadcast.unibremen.sendbroadcast;
 
 import android.app.Activity;
-import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
+//implicite
 public class SendBroadcastActivity extends Activity {
 
     @Override
@@ -21,8 +19,7 @@ public class SendBroadcastActivity extends Activity {
         Intent intent = new Intent();
         intent.setAction("_myAction_");
         intent.addCategory("_myCategory_");
-        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
-        intent.putExtra("_myIdentifier_", "infosblabla");
+        intent.putExtra("_myIdentifier_", "geheime infos");
         sendBroadcast(intent);
     }
 }
