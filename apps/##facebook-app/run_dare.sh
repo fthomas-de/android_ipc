b29aff14b4b03d3ddf2bdc39084a97687dbdd3b6
Florian Thomas <fthomas@tzi.de>
2015-06-21 14:44:49 +0200

    gedared

---
+++ apps/##facebook-app/run_dare.sh
@@ -0,0 +1,1 @@
+dare -d ~/git/android_ipc/apps/##facebook-app/dare_output/ ~/git/android_ipc/apps/##facebook-app/app/app-release.apk
---
+++ apps/##facebook-app/run_epicc.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+java -Xmx8g -jar /opt/epicc-0.1/epicc-0.1.jar -apk ~/git/android_ipc/apps/##facebook-app/app/app-release.apk -android-directory ~/git/android_ipc/apps/##facebook-app/dare_output/retargeted/app-release -cp /opt/epicc-0.1/android.jar -icc-study ~/git/android_ipc/apps/##facebook-app/
+mv ~/git/android_ipc/apps/##facebook-app/app-release.txt ~/git/android_ipc/icc_out/##facebook-app.txt
